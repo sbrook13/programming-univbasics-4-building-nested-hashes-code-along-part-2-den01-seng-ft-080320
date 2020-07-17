8f7c5e7de0193c19749009e6e076a7850c6a92be
Shelley Brook <sbrook13@gmail.com>
2020-07-17 16:54:13 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,8 +51,8 @@ def add_characters
    }
   epic_tragedy[:additional_characters] = [
     {:name => "Prince Escalus"},
-    {:name => "Apothecary"
-  }
+    {:name => "Apothecary"}
+    ]
 
    epic_tragedy
 end
